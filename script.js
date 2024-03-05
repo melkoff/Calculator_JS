105cc19b1c2311bc1862959b91c17d4033b2be8a
melkoff <vasamelko@gmail.com>
2024-03-05 13:42:14 +0200

    test increase decrease reset

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function myFunction() {
-   alert("Hello World!");
-}
-myFunction();
+const decreaseBtn = document.getElementById('decreaseBtn');
+const resetBtn = document.getElementById('resetBtn');
+const increaseBtn = document.getElementById('increaseBtn');
+const countLabel = document.getElementById('countLabel');
+
+let count = 0;
+
+decreaseBtn.addEventListener('click', () => {
+   count--;
+   countLabel.textContent = count;
+});
+
+resetBtn.addEventListener('click', () => {
+   count = 0;
+   countLabel.textContent = count;
+});
+
+increaseBtn.addEventListener('click', () => {
+   count++;
+   countLabel.textContent = count;
+});
