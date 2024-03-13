8565ea194951187a3e663185f03568868c2d93a8
melkoff <vasamelko@gmail.com>
2024-03-13 12:14:17 +0200

    guess number

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-const email = "vasamelko@gmail.com";
+// number guessing game
 
-let username = email.slice(0, email.indexOf("@"));
-let extension = email.slice(email.indexOf("@"));
-let find = email.slice(email.charAt("@") + 1);
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
-console.log(username);
-console.log(extension);
-console.log(find);
+let attempts = 0;
+let guess;
+let running = true;
+
+
+while (running) {
+   guess = window.prompt(`Вгадай число між ${minNum} та ${maxNum}`);
+   guess = Number(guess);
+
+   if (isNaN(guess)) {
+      window.alert("Введіть коректне число");
+   }
+   else if (guess < minNum || guess > maxNum) {
+      window.alert("Введіть коректне число від 1 до 100");
+   }
+   else {
+      attempts++;
+      if (guess < answer) {
+         window.alert("Надто мало. Спробуйте ще раз");
+      }
+      else if (guess > answer) {
+         window.alert("Надто багато. Спробуйте ще раз");
+      }
+      else {
+         window.alert(`Вітаємо! Ви вгадали за ${attempts} спроб`);
+         running = false;
+      }
+   }
+}
