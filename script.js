23937c04aa2979dafaab6da0bb7ff2ed384aba7f
melkoff <vasamelko@gmail.com>
2024-03-13 14:26:26 +0200

    conversation temperature

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-const email = "vasamelko@gmail.com";
+const textBox = document.getElementById("textBox");
+const FAHRENHEIT = document.getElementById("toFahrenheit");
+const CELSIUM = document.getElementById("toCelsius");
+const result = document.getElementById("result");
 
-let username = email.slice(0, email.indexOf("@"));
-let extension = email.slice(email.indexOf("@"));
-let find = email.slice(email.charAt("@") + 1);
+let temperature;
 
-console.log(username);
-console.log(extension);
-console.log(find);
+function convert() {
+
+   if (FAHRENHEIT.checked) {
+      temperature = Number(textBox.value);
+      temperature = temperature * 9 / 5 + 32;
+      result.textContent = temperature.toFixed(1) + "°F градусів за фаренгейтом";
+   }
+   else if (CELSIUM.checked) {
+      temperature = Number(textBox.value);
+      temperature = (temperature - 32) * 5 / 9;
+      result.textContent = temperature.toFixed(1) + "°C градусів за цельсієм";
+   }
+   else {
+      result.textContent = "Виберіть один з пунктів";
+   }
+
+}
