d3c97b7d236118ee804c793d6761c556d48f0b9f
melkoff <vasamelko@gmail.com>
2024-03-27 11:54:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 sum(displayContent, 10, 5);
-
 function sum(callback, a, b) {
+   //Виконуємо код
    let result = a + b;
    callback(result);
 }
 
 
+// Виклик функції sum з параметром callback та значеннями аргументів 10 та 5
 function displayContent(result) {
    document.getElementById('titleResult').innerHTML = result;
 }
