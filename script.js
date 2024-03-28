261f2accda4168297b2ea04f02d0f9a698673f5d
melkoff <vasamelko@gmail.com>
2024-03-28 11:52:27 +0200

    map examples

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-const fullName = ["Я", "Одна", "Строка"];
-function together(...strings) {
+const members = ['Vasa', 'Ira', 'Nazar'];
 
-   return strings.join(" ");
+const membersUpper = members.map(upperCase);
+const membersLower = members.map(lowerCase);
 
+console.log(membersLower);
+
+function upperCase(element) {
+   return element.toUpperCase();
 }
 
-console.log(...fullName);
+function lowerCase(element) {
+   return element.toLowerCase();
+}
 
 
 
+// ======================================== DATES ============================== //
 
-// const fullName = ["Я", "Одна", "Строка"];
+const dates = ['1997-06-03', '2000-10-20', '2024-03-28'];
+const formattedDates = dates.map(formatDate);
 
-// function together(...strings) {
-//    return strings.join(" ");
-// }
+console.log(formattedDates)
 
-// const textResult = document.getElementById("result");
-// textResult.innerHTML = together(...fullName);
+function formatDate(element) {
+   const parts = element.split('-');
+   return `${parts[2]}.${parts[1]}.${parts[0]}`
+}
