ebfc885061f893e233f82d211d271203c7a5b329
melkoff <vasamelko@gmail.com>
2024-04-01 15:36:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,40 @@ productOne.displayProduct();
 
 const totalPrice = productOne.calculateTotal(salesTax);
 console.log(`Total price (with tax): $${totalPrice.toFixed(2)}`);
+
+
+
+// ========================================= Another Example ===================== //
+console.log(" ========================================= Another Examples ===================== ")
+
+class User {
+
+   static userCount = 0;
+
+   constructor(username) {
+      this.username = username;
+      User.userCount++;
+   }
+
+   sayHello() {
+      console.log(`Hello, my user name is ${this.username}`)
+   }
+
+   static totalUser() {
+      console.log(`Total users count is ${User.userCount}`);
+   }
+
+}
+
+// Get constructon username
+const user1 = new User("Vasa");
+const user2 = new User("Iren");
+const user3 = new User("Nazar");
+
+// Show username with sayHello function
+user1.sayHello();
+user2.sayHello();
+user3.sayHello();
+
+// Show all amount of users
+User.totalUser();
