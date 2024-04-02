ff93765857b80e695e309f0ae6253db14e6e6c58
melkoff <vasamelko@gmail.com>
2024-04-02 17:25:23 +0300

    getter and setter examples

--- script.js
+++ script.js
@@ -1,68 +1,115 @@
-class Transport {
+class Rectangle {
 
-   constructor(name, color, speed) {
-      this.name = name;
-      this.color = color;
-      this.speed = speed;
+   constructor(width, height) {
+      this.width = width;
+      this.height = height;
+   }
+
+   // setters makes property writeable
+   set width(newWidth) {
+      if (newWidth > 0) {
+         this._width = newWidth;
+      }
+      else {
+         console.error("Width must be more than zero!")
+      }
+   }
+
+   set height(newHeight) {
+      if (newHeight > 0) {
+         this._height = newHeight;
+      }
+      else {
+         console.error("Height must be more than zero!")
+      }
+   }
+
+   // getters makes property readable
+   get width() {
+      return `${this._width.toFixed(1)}cm`;
+   }
+
+   get height() {
+      return `${this._height.toFixed(1)}cm`;
+   }
+
+   get area() {
+      return `${(this._width * this._height).toFixed(1)}cm`;
    }
 
 }
 
-class Car extends Transport {
+const rectangle = new Rectangle(10, 4);
 
-   constructor(name, color, speed, drive) {
-      super(name, color, speed); // We use the (super) from parent constructor
-      this.drive = drive;
+console.log(rectangle.width);
+console.log(rectangle.height);
+console.log(rectangle.area);
+
+
+// ==================================================== Another Example ==================================== //
+console.log("Another Example")
+
+
+class Persone {
+
+   constructor(firstName, lastName, age) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
    }
 
-   // add new method
-   ride() {
-      console.log(`This ${this.name} can use road`);
+   // setters makes property writeable
+   set firstName(newFirstName) {
+      if (typeof newFirstName === "string" && newFirstName.length > 0) {
+         this._firstName = newFirstName;
+      }
+      else {
+         console.error("First name must a non-empty string")
+      }
+   }
+
+   set lastName(newLastName) {
+      if (typeof newLastName === "string" && newLastName.length > 0) {
+         this._lastName = newLastName;
+      }
+      else {
+         console.error("Last name must a non-empty string")
+      }
+   }
+
+   set age(newAge) {
+      if (typeof newAge === "number" && newAge >= 0) {
+         this._age = newAge;
+      }
+      else {
+         console.error("Age must be a non-negative number")
+      }
+   }
+
+
+   // getters makes property readable
+   get firstName() {
+      return this._firstName.charAt(0).toUpperCase() + this._firstName.slice(1);
+   }
+
+   get lastName() {
+      return this._lastName;
+   }
+
+   get age() {
+      return this._age;
+   }
+
+   get fullName() {
+      return this._firstName + " " + this._lastName;
    }
 
 }
 
+const persone = new Persone("vasa", "melko", 26);
 
-class Plane extends Transport {
+console.log(persone.firstName);
+console.log(persone.lastName);
+console.log(persone.age);
 
-   constructor(name, color, speed, fly) {
-      super(name, color, speed); // We use the (super) from parent constructor
-      this.fly = fly;
-   }
-
-   // add new method
-   flying() {
-      console.log(`This ${this.name} can use sky`);
-   }
-
-}
-
-
-class Ship extends Transport {
-
-   constructor(name, color, speed, swim) {
-      super(name, color, speed); // We use the (super) from parent constructor
-      this.swim = swim;
-   }
-
-   // add new method
-   swiming() {
-      console.log(`This ${this.name} can use sea`);
-   }
-
-}
-
-
-const car = new Car("Ford", "Red", 40, true);
-const plane = new Plane("Boing", "White", 350, true);
-const ship = new Ship("Poseidon", "Blue", 120, true)
-
-console.log(car.name);
-console.log(car.color);
-console.log(car.speed);
-
-console.log(car.drive ? true : false);
-
-car.ride();
-plane.flying();
-ship.swiming();
+console.log(persone.fullName);
