5013e9c5902da9dd011699c7b05a6a0d8c2c717d
melkoff <vasamelko@gmail.com>
2024-04-05 18:46:26 +0300

    dates examples

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
-let numbers = [1, 2, 5, 9, 10, 3, 7, 4, 6, 8];
+const date = new Date();
+console.log(date);
 
-numbers.sort((a, b) => b - a);
-
-console.log(numbers)
-
-// ===================================== Another example ============================== //
-console.log("===================================== Another example ==============================")
-
-const people = [
-   { name: "bob", age: 20, gpa: 3.4 },
-   { name: "carl", age: 30, gpa: 3.9 },
-   { name: "alice", age: 25, gpa: 2.9 },
-   { name: "david", age: 27, gpa: 4.0 },
-   { name: "mike", age: 22, gpa: 2.2 }
-]
+// Get current data( year, month, day)
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+const hour = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+const dayOfWeek = date.getDay();
 
 
-// sort by alphabet 1 example
-people.sort((a, b) => a.name.localeCompare(b.name));
-
-
-// sort by alphabet 2 example
-// people.sort((a, b) => {
-//    if (a.name < b.name) {
-//       return -1
-//    } else if (a.name > b.name) {
-//       return 1
-//    } else {
-//       return 0
-//    }
-// });
-
-console.log(people);
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(hour);
+console.log(minutes);
+console.log(seconds);
+console.log(dayOfWeek);
