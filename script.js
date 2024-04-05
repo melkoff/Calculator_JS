1ad8819df04a459ac7b55938390bef952c7664d8
melkoff <vasamelko@gmail.com>
2024-04-05 19:34:17 +0300

    set timeout example

--- script.js
+++ script.js
@@ -1,28 +1,8 @@
-console.log('======================== Another Example =========================');
+function showResult() {
 
-function createCounter() {
-   let score = 0;
+   let result = document.getElementById('result');
 
-   function increaseScore(points) {
-      score += points;
-      console.log(`+${points}`);
-   }
-
-   function decreaseScore(points) {
-      score -= points;
-      console.log(`-${points}`);
-   }
-
-   function getScore() {
-      return score;
-   }
-
-   return { increaseScore, decreaseScore, getScore };
+   result.style.display = 'block';
 }
 
-const COUNTER = createCounter();
-
-COUNTER.increaseScore(7);
-COUNTER.increaseScore(7);
-COUNTER.decreaseScore(13);
-console.log(`The final score is ${COUNTER.getScore()} pts`);
+setTimeout(showResult, 3000);
