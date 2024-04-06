f06637507b64bbb2cd7e52bfdab59fdc8f02c88f
melkoff <vasamelko@gmail.com>
2024-04-06 13:32:22 +0300

    timer example

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
-function updateClock() {
+const display = document.getElementById('display');
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
 
-   const currentTime = new Date();
-   const hours = currentTime.getHours().toString().padStart(2, 0);
-   const minutes = currentTime.getMinutes().toString().padStart(2, 0);
-   const seconds = currentTime.getSeconds().toString().padStart(2, 0);
-   const timeString = `${hours}:${minutes}:${seconds}`;
-   document.getElementById('clock').textContent = timeString;
+function start() {
 
+   if (!isRunning) {
+      startTime = Date.now() - elapsedTime;
+      timer = setInterval(update, 10);
+      isRunning = true;
+   }
 }
 
-updateClock();
-setInterval(updateClock, 1000)
+function stop() {
+
+   if (isRunning) {
+      clearInterval(timer);
+      elapsedTime = Date.now() - startTime;
+      isRunning = false;
+   }
+}
+
+function reset() {
+   clearInterval(timer);
+   startTime = 0;
+   elapsedTime = 0;
+   isRunning = false;
+   display.textContent = "00:00:00:00"
+}
+
+function update() {
+
+   const currentTime = Date.now();
+   elapsedTime = currentTime - startTime;
+
+   let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+   let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+   let seconds = Math.floor(elapsedTime / 1000 % 60);
+   let miliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+
+   hours = String(hours).padStart(2, 0);
+   minutes = String(minutes).padStart(2, 0);
+   seconds = String(seconds).padStart(2, 0);
+   miliseconds = String(miliseconds).padStart(2, 0);
+
+
+   display.textContent = `${hours}:${minutes}:${seconds}:${miliseconds}`;
+}
